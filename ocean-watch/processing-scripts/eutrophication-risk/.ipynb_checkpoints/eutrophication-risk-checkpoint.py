e1106f04b9aaabb238b29ae8565069770a09299b
rachelthoms <rachel.e.thoms@gmail.com>
2021-09-28 17:41:07 -0400

    new processing scripts and minor changes

---
+++ ocean-watch/processing-scripts/eutrophication-risk/.ipynb_checkpoints/eutrophication-risk-checkpoint.py
@@ -0,0 +1,89 @@
+from cartoframes.auth import set_default_credentials
+from cartoframes import read_carto, to_carto
+import geopandas as gpd
+import pandas as pd
+import os
+import logging 
+from shapely import geometry
+
+
+# set up logging
+
+# get top-level logger object
+logger = logging.getLogger()
+for handler in logger.handlers: logger.removeHandler(handler)
+# manually set level 
+logger.setLevel(logging.INFO)
+# print to console
+console = logging.StreamHandler()
+logger.addHandler(console)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# authenticate carto account 
+CARTO_USER = os.getenv('CARTO_WRI_RW_USER')
+CARTO_KEY = os.getenv('CARTO_WRI_RW_KEY')
+set_default_credentials(username=CARTO_USER, base_url="https://{user}.carto.com/".format(user=CARTO_USER),api_key=CARTO_KEY)
+                        
+logger.info('Pull ICEP Basins from Carto table')
+# read in Aqueduct Index Coastal Eutrophication Potentional Data (ICEP)
+gdf_icep= read_carto('wat_059_aqueduct_coastal_eutrophication_potential')
+# project to the a projected CRS
+gdf_icep.to_crs('epsg:3035', inplace=True)
+
+logger.info('Calculate basin centroids')
+# calculate the centroids for each basin polygon
+gdf_icep['points'] = gdf_icep['the_geom'].centroid
+# remname the points column to geometry
+gdf_icep.rename(columns = {'points': 'geometry'}, inplace = True)
+# drop the polygon geometry
+gdf_icep.drop(columns=['the_geom'], inplace= True)
+# create a new geodataframe with the centroids as the geometry and set CRS
+gdf_icep =gpd.GeoDataFrame(gdf_icep)
+gdf_icep.set_crs('epsg:3035', inplace=True)
+# reproject CRS to geographic CRS to match gadm polygons
+gdf_icep.to_crs('epsg:4326', inplace=True)
+
+
+logger.info('Pull country boundaries from Carto table')
+# read in the GADM country boundaries used in the OW geostore
+gdf_gadm = read_carto("gadm36_0")
+# rename the_geom column to geometry
+gdf_gadm.rename(columns = {'the_geom': 'geometry'}, inplace = True)
+
+logger.info('Join centroids with country boundaries')
+# join the centroid and country polygons (intersection)
+gdf= gpd.sjoin(gdf_icep,gdf_gadm,how = 'inner')
+# group and aggregate the dataframe by the country and risk level, counting the number of centroid    
+gdf = gdf.groupby(['gid_0','cep_label']).agg({'name_0': 'first', 'pfaf_id':'nunique'}).reset_index()
+# rename count colum
+gdf.rename(columns = {'pfaf_id': 'count'}, inplace = True)
+# subset the dataframe to remove geometry
+df = gdf[['gid_0', 'name_0', 'cep_label', 'count']]
+
+# create a column for the proportion
+# group by country and risk level
+df_grouped = df.groupby(['gid_0']).agg({'count':'sum'}).reset_index()
+df_grouped.rename(columns = {'count': 'country_total'}, inplace = True)
+df = pd.merge(df,df_grouped[['gid_0', 'country_total']], on='gid_0', how='left')
+df['proportion'] = df['count']/df['country_total']
+
+
+# calculate statistics for the world
+# totals by risk level
+df_global = df.groupby(['cep_label']).agg({'count':'sum'}).reset_index()
+# global total
+total_basins = sum(df_global['count'])
+# proportion
+df_global['proportion'] = df_global['count']/total_basins
+
+df_global['gid_0'] = 'GLB'
+df_global['name_0'] = 'Global'
+df_global['country_total'] = total_basins
+
+df = pd.concat([df,df_global]).reset_index()
+df['name'] = df.name_0.astype(str)
+
+
+logger.info('Upload dataframe to Carto')
+# upload data frame to Carto
+to_carto(df, 'ocn_calcs_014_eutrophication_risk', if_exists='replace')
---
+++ ocean-watch/processing-scripts/global-rivers/harvest-rivers.py
@@ -0,0 +1,107 @@
+import osm2geojson 
+import requests
+import json
+import fiona
+from fiona.crs import from_epsg
+import time
+import logging
+
+
+# set up logging
+
+# get top-level logger object
+logger = logging.getLogger()
+for handler in logger.handlers: logger.removeHandler(handler)
+# manually set level 
+logger.setLevel(logging.INFO)
+# print to console
+console = logging.StreamHandler()
+logger.addHandler(console)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Define grid to iterate through a series bboxs that cover world starting from the top left and moving to the bottom right coordinate
+
+# Define the start coordinate
+x_start = -180
+y_start = 90
+# Define the grid size
+x_size = 10
+y_size = 10
+# Define the stop coordinate
+x_stop = 180
+y_stop = -90
+
+# Set up to query Overpass API
+# Define the url for the hosted Overpass API (other urls can be found at https://wiki.openstreetmap.org/wiki/Overpass_API)
+overpass_url = "https://lz4.overpass-api.de/api/interpreter"
+
+# template for Overpass QL query
+template = """
+[out:json]
+[timeout:180];
+way{bbox}[waterway=river]["name"];
+(._;>;);out;
+"""
+
+# Define a list to store the elements
+rivers=[]
+
+# Iterate through the coordinates of the bbox grid, request the defined query, and save the geometries of the resulting elements
+
+# Start at the left-most x coordinate
+x_left = x_start
+# Move from left to right until the left most x-cordinate is reached
+while x_left <= x_stop - x_size:
+    # Start at the top y coordinate
+    y_top = y_start
+    # move top to bottom until the bottom-most y coodinate is reached
+    while y_top >= y_stop + y_size:
+        # Define the resulting bbox
+        bbox_tuple = (y_top-y_size, x_left, y_top, x_left+x_size)
+        logger.info("Getting rivers for: " + str(bbox_tuple))
+        # Attempt to query the data, moving on after 10 failed attempts
+        for attempt in range(10):                
+            try: 
+                # Format the query with the current bbox
+                overpass_query= template.format(time=time[attempt],bbox=bbox_tuple)
+                # Request elements from the query
+                response = requests.get(overpass_url, params={'data': overpass_query,}, headers = {'User-agent': 'wriuser'})
+                # save the resulting json
+                data = response.json()  
+                # convert the data from json to geojson format                  
+                geojson= osm2geojson.json2geojson(data)
+                # move on to next bbox
+                break
+            except: 
+                logger.warning("error " + str(response.status_code) + "- retrying in 3 minutes")
+                # wait three minutes before re-requesting 
+                time.sleep(180)                
+        else:
+            logger.error("unable to fetch data for" + str(bbox_tuple))
+            # move on to next bbox after 10 failed attemps
+            break            
+        for way in geojson['features']: 
+            rivers.append(way)
+        logger.info(str(len(geojson['features'])) + " rivers added. Total rivers: " + str(len(rivers)))
+        # move to the bbox lower
+        y_top = y_top - y_size 
+    # move to the bbox to the right           
+    x_left = x_left + x_size
+
+# Save the resulting elements as a shapefile
+
+# Define the schema
+schema = {'geometry': 'LineString', 'properties': {'Name':'str:80'}}
+# File to save the elements
+shapeout = "osm_rivers_edit.shp"
+# Write each element into the shapefile
+with fiona.open(shapeout, 'w',crs=from_epsg(4326),driver='ESRI Shapefile', schema=schema) as output:
+    for way in rivers:
+        # the shapefile geometry use (lon,lat) 
+        line = {'type': 'LineString', 'coordinates': way['geometry']['coordinates']}
+        # the shapefile properties
+        prop = {'Name': way['properties']['tags']['name']}
+        try:
+            output.write({'geometry': line, 'properties':prop})  
+        except:
+            logger.warning("could not add " + prop["Name"])
